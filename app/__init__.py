49918bd7606a80d1d7ff377292a517713e9ece05
alig2039 <alig2039@yahoo.com>
2022-06-15 18:18:15 +0000

    create sqlite file database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from .ui.routes import ui_bp
+from .ui.utils import db
+
 
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
 
     app.register_blueprint(ui_bp)
-
 
     return app
